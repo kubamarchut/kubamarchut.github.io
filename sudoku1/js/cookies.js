d0da5844f92af8429f4f0205aa041ff4d88d3851
kubamarchut <43681491+kubamarchut@users.noreply.github.com>
2019-05-27 08:10:42 +0200

    Add files via upload

--- sudoku1/js/cookies.js
+++ sudoku1/js/cookies.js
@@ -18,4 +18,5 @@ function setCookie(cname,cvalue,exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires=" + d.toGMTString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("New cookie");
 }
